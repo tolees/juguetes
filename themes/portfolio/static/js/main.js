a99d765c9c262c09baa61046428963f16965d393
myself <none@nonecom>
2020-12-13 01:19:39 +0100

    Myupdate

--- themes/portfolio/static/js/main.js
+++ themes/portfolio/static/js/main.js
@@ -3,7 +3,7 @@ $(function() {
 })
 
 
-$('img').removeAttr('width').removeAttr('height').addClass('img-responsive');
+$('img').removeAttr('width').removeAttr('height');
 
 function initializeLazyLoadOfImages() {
     var bLazy = new Blazy();
